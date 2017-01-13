56b6dc524ee161ca34ab1166655aa25a2a98f5b9
GtrCraft <nickvanbokhorst@hotmail.com>
2017-01-13 16:33:30 +0100

    Aosip Bringup

--- device.mk
+++ device.mk
@@ -60,6 +60,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_PACKAGES += \
     init.target.rc
 
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
 # Thermal
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/configs/thermal-engine-lux.conf:system/etc/thermal-engine-lux.conf
